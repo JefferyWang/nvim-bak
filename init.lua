9884412388704482cfbd5b1ada4bc3ca2d81e0ce
jefferwang <jefferwang@tencent.com>
2022-09-23 21:05:51 +0800

    add

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- 基础配置
+require("basic")
+
+-- 快捷键绑定
+require("keybindings")
+
+-- 插件
+require("plugins")
+
+-- 主题设置
+require("colorscheme")
+
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+require("plugin-config.indent-blankline")
+
+-- 内置LSP
+require("lsp.setup")
+require("lsp.cmp")
+require("lsp.ui")
+
